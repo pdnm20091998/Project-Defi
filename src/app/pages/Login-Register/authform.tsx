854495a370c17b216091e255094c9c7e4dd50adf
anhladrt <67309998+anhladrt@users.noreply.github.com>
2021-10-07 00:32:36 +0700

    Hoang login day2 finish

--- src/app/pages/Login-Register/authform.tsx
+++ src/app/pages/Login-Register/authform.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Switch, Route, Link, useRouteMatch } from 'react-router-dom';
 import Frame from './assets/img/Frame.png';
 import Title from './Title/Title';
 import Login from './Login/Login';
 import Signup from './Signup/Sigup';
-import { Container, Row, Col, Tabs, Tab } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import styled from 'styled-components/macro';
 import Navbar from './Navbar/Navbar';
 
@@ -14,8 +15,48 @@ const Img = styled.img`
     margin-left: 52px;
   }
 `;
-
+const Tab = styled.div<{
+  color?: string;
+  borderbottom?: any;
+  fontweight?: number;
+}>`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 30px;
+  line-height: 37px;
+  margin-bottom: 24px;
+  display: inline-block;
+  .signup {
+    color: #fff;
+    border-bottom: none;
+    padding-bottom: 2px;
+    text-decoration: none;
+    margin-right: 48px;
+  }
+  .login {
+    color: #fff;
+    border-bottom: none;
+    padding-bottom: 2px;
+    text-decoration: none;
+  }
+  .active {
+    color: #dba83d;
+    border-bottom: 3px solid #dba83d;
+    font-weight: 600;
+  }
+`;
 export default function AuthForm() {
+  let { path, url } = useRouteMatch();
+  const [activeSignup, setActiveSignup] = useState(false);
+  const [activeLogin, setActiveLogin] = useState(true);
+  const TabSignup = () => {
+    setActiveSignup(true);
+    setActiveLogin(false);
+  };
+  const TabLogin = () => {
+    setActiveSignup(false);
+    setActiveLogin(true);
+  };
   return (
     <>
       <Navbar />
@@ -27,18 +68,37 @@ export default function AuthForm() {
             xs={{ span: 12, order: 2 }}
             lg={{ span: 6, order: 1 }}
           >
-            <Tabs
-              defaultActiveKey="profile"
-              id="uncontrolled-tab-example"
-              className="mb-3 mt-5"
-            >
-              <Tab eventKey="home" title="Home">
-                <Signup />
-              </Tab>
-              <Tab eventKey="profile" title="Profile">
+            <Tab>
+              <span>
+                <Link
+                  onClick={TabSignup}
+                  className={activeSignup ? 'signup active' : 'signup'}
+                  to={`${url}/tab=1`}
+                >
+                  Sign up
+                </Link>
+              </span>
+              <span>
+                <Link
+                  onClick={TabLogin}
+                  className={activeLogin ? 'login active' : 'login'}
+                  to={`${url}/tab=2`}
+                >
+                  Log in
+                </Link>
+              </span>
+            </Tab>
+            <Switch>
+              <Route exact path={path}>
                 <Login />
-              </Tab>
-            </Tabs>
+              </Route>
+              <Route path={`${path}/tab=2`}>
+                <Login />
+              </Route>
+              <Route path={`${path}/tab=1`}>
+                <Signup />
+              </Route>
+            </Switch>
           </Col>
           <Col xs={{ span: 12, order: 1 }} lg={{ span: 6, order: 2 }}>
             <Img src={Frame} alt="Frame" />
