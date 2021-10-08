059c7300f51fe3719ad0dee5563d31a31918467f
Namcn111999 <46323026+Namcn111999@users.noreply.github.com>
2021-10-08 08:10:45 +0700

    Nam-day3

--- src/app/pages/Home/HomePage/Content/Content.tsx
+++ src/app/pages/Home/HomePage/Content/Content.tsx
@@ -1,1 +1,115 @@
 import styled from 'styled-components/macro';
+import fast_time from './asset/fast-time.svg';
+import arrow from './asset/Group-arrow.svg';
+import money from './asset/money-management.svg';
+import wallet from './asset/Group-wallet.svg';
+import { Container, Row, Col } from 'react-bootstrap';
+import './sass/content.scss';
+
+const DivContent = styled.div`
+  padding: 60px 0;
+  width: 100%;
+  background: rgb(26, 26, 35);
+`;
+
+const Title = styled.h3`
+  font-size: 36px;
+  color: #dba83d;
+  font-weight: 600;
+  line-height: 44px;
+  text-align: center;
+  margin: 0;
+  margin-bottom: 100px;
+`;
+
+const Image = styled.img`
+  width: 80px;
+  height: 80px;
+`;
+const Error_img = 'No picture';
+
+const ContentItem = styled.div`
+  text-align: center;
+  color: #ffffff;
+  img {
+    margin-bottom: 34px;
+  }
+  .content__title {
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 24px;
+    text-transform: capitalize;
+  }
+
+  .content__info {
+    margin-top: 20px;
+    font-size: 16px;
+    line-height: 20px;
+  }
+`;
+
+const Content = () => {
+  return (
+    <DivContent className="content">
+      <Container>
+        <Row>
+          <Title className="title">Why Borrow With Us?</Title>
+        </Row>
+        <Row>
+          <Col sm={12} lg={3} className="content__col">
+            <ContentItem className="content__item">
+              <Image src={fast_time} alt={Error_img} />
+              <div className="content__text">
+                <h3 className="content__title">Quicker than the bank</h3>
+                <p className="content__info">
+                  Banks are yesterdays news. Get the money you need without
+                  having to wait.
+                </p>
+              </div>
+            </ContentItem>
+          </Col>
+          <Col sm={12} lg={3} className="content__col">
+            <ContentItem className="content__item">
+              <Image src={arrow} alt={Error_img} />
+              <div className="content__text">
+                <h3 className="content__title">Better interest rates</h3>
+                <p className="content__info">
+                  P2P lending lets you negotiate with the lender to arrange the
+                  best possible interest rates
+                </p>
+              </div>
+            </ContentItem>
+          </Col>
+          <Col sm={12} lg={3} className="content__col">
+            <ContentItem className="content__item">
+              <Image src={money} alt={Error_img} />
+              <div className="content__text">
+                <h3 className="content__title">
+                  Finance assets without selling
+                </h3>
+                <p className="content__info">
+                  Put your assets to work so you can continue to meet your
+                  financial obligations.
+                </p>
+              </div>
+            </ContentItem>
+          </Col>
+          <Col sm={12} lg={3} className="content__col">
+            <ContentItem className="content__item">
+              <Image src={wallet} alt={Error_img} />
+              <div className="content__text">
+                <h3 className="content__title">No credit checks</h3>
+                <p className="content__info">
+                  Borrow without the background checks. Anyone can get the
+                  financing they need.
+                </p>
+              </div>
+            </ContentItem>
+          </Col>
+        </Row>
+      </Container>
+    </DivContent>
+  );
+};
+
+export default Content;
--- src/app/pages/Home/HomePage/Explore/Explore.tsx
+++ src/app/pages/Home/HomePage/Explore/Explore.tsx
@@ -1,1 +1,146 @@
 import styled from 'styled-components/macro';
+import Slider from 'react-slick';
+import { Container } from 'react-bootstrap';
+import explore_one from '../Explore/asset/Rectangle_one.png';
+import explore_two from '../Explore/asset/Rectangle_two.png';
+import explore_three from '../Explore/asset/Rectangle_three.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
+const DivCarousel = styled.div`
+  padding: 60px 15px 80px 15px;
+  .slide__item {
+    padding: 0 10px;
+  }
+  .slide__text {
+    color: white;
+    .slide__title {
+      margin: 12px 0 8px 0;
+      font-weight: 600;
+      font-size: 20px;
+      line-height: 24px;
+      text-transform: capitalize;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      overflow: hidden;
+    }
+    .slide__param {
+      font-style: normal;
+      font-weight: normal;
+      font-size: 16px;
+      line-height: 20px;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 1;
+      overflow: hidden;
+    }
+  }
+  img {
+    max-width: 100%;
+  }
+  span {
+    font-weight: 500;
+    font-size: 16px;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    padding: 0 10px 12px 0;
+    svg {
+      margin-left: 6px;
+    }
+  }
+`;
+
+const TitleExplore = styled.h3`
+  margin-bottom: 100px;
+  color: #dba83d;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: capitalize;
+`;
+
+const SeeAll = styled.span`
+  color: #ffffff;
+`;
+
+const Explore = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <Container>
+      <DivCarousel>
+        <TitleExplore>explore featured pawnshops</TitleExplore>
+        <SeeAll>
+          See all
+          <FontAwesomeIcon icon={faArrowRight} />
+        </SeeAll>
+        <Slider {...settings}>
+          <div className="slide__item">
+            <img src={explore_one} alt="" />
+            <div className="slide__text">
+              <h3 className="slide__title">Pawnshop America</h3>
+              <p className="slide__param">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+          <div className="slide__item">
+            <img src={explore_two} alt="" />
+            <div className="slide__text">
+              <h3 className="slide__title">Pawnshop America</h3>
+              <p className="slide__param">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+          <div className="slide__item">
+            <img src={explore_three} alt="" />
+            <div className="slide__text">
+              <h3 className="slide__title">Pawnshop America</h3>
+              <p className="slide__param">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+        </Slider>
+      </DivCarousel>
+    </Container>
+  );
+};
+
+export default Explore;
