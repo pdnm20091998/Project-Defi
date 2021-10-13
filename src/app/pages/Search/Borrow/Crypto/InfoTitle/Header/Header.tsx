9fbf33094fc05033e581829451cade71c420585a
Namcn111999 <46323026+Namcn111999@users.noreply.github.com>
2021-10-13 10:30:06 +0700

    Nam-day7

--- src/app/pages/Search/Borrow/Crypto/InfoTitle/Header/Header.tsx
+++ src/app/pages/Search/Borrow/Crypto/InfoTitle/Header/Header.tsx
@@ -1,1 +1,22 @@
 import styled from 'styled-components/macro';
+const HeaderCrypto = props => {
+  console.log(props);
+
+  return (
+    <div className="header__crypto">
+      <div className="header__image">
+        <img
+          className="header__imgCrypto"
+          src={props.picture}
+          alt="NotPicture"
+        />
+      </div>
+      <div className="header__content">
+        <div className="header__title">{props.title}</div>
+        <div className="header__text">{props.text}</div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderCrypto;
